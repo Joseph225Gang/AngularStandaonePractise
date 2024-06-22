2609dace9cdd8f5f069a6cb2aee2f4e7c456c7bc
MSI\lai22 <lai225joseph@gmail.com>
2024-06-22 19:17:45 +0800

    ngcontent

--- src/app/font-size/font-size.component.ts
+++ src/app/font-size/font-size.component.ts
@@ -1,4 +1,4 @@
-import { Attribute, Component, EventEmitter, Input, Output } from '@angular/core';
+import { Attribute, Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-font-size',
@@ -7,10 +7,12 @@ import { Attribute, Component, EventEmitter, Input, Output } from '@angular/core
   templateUrl: './font-size.component.html',
   styleUrl: './font-size.component.css'
 })
-export class FontSizeComponent {
+export class FontSizeComponent implements OnChanges{
   @Input() size!: number;
 
   @Output() sizeChange = new EventEmitter<number>();
+
+  @Output() stringChange = new EventEmitter<string>();
 
   constructor(@Attribute('color') public color: string) {}
 
@@ -18,4 +20,9 @@ export class FontSizeComponent {
     this.size += value;
     this.sizeChange.emit(this.size);
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+      if(changes.size)
+        this.stringChange.emit(`${changes.size.currentValue}pt`);
+    }
 }
